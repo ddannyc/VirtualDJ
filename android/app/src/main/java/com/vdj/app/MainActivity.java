81244e50b952019ac782b130efa3bee8c5b21bf3
weilong <chenweilong@yeezon.com>
2022-10-19 16:14:44 +0800

    refactor: config capacitor

---
+++ android/app/src/main/java/com/vdj/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vdj.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vdj.app',
+  appName: 'vdj',
+  webDir: 'dist/VDJ',
+  bundledWebRuntime: false
+};
+
+export default config;
